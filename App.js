915140a4362a0747089e7d6fba9dc4c255b718d3
fatihyalcn <fatihyalcn9332@gmail.com>
2022-02-17 01:43:12 +0100

    updated

---
+++ App.js
@@ -0,0 +1,50 @@
+let numberArea = document.getElementById("number-area");
+let submitButton = document.getElementById("submit-button");
+let result = document.getElementById("result");
+let attempt = document.getElementById("attempt");
+let attemptText = document.getElementById("text2");
+let attemptDiv = document.querySelector(".attempt")
+
+attempt.value = 10;
+
+let y = 44;
+
+console.log(y);
+
+submitButton.addEventListener("click", ()=>{
+    
+    if(numberArea.value == y){
+        result.innerHTML = "Congrats! You found the number in your " + (10 - (attempt.value - 1)) + ". trial.";
+        // attemptDiv.style.display = "none";
+        document.body.style.backgroundImage = "url('https://img.freepik.com/free-photo/picture-blue-purple-fireworks-during-night_181624-15999.jpg?w=826')";
+    }
+
+    else if(numberArea.value < y)
+
+        result.innerHTML = `Enter a number between ${numberArea.value} and 100`
+
+    else if(numberArea.value > 100){
+        alert("The number should be between 0-100")
+    }
+
+    else{
+        result.innerHTML = `Enter a value between 0 and ${numberArea.value}`
+    }
+
+});
+
+submitButton.addEventListener("click", ()=>{
+    
+    if(numberArea.value != y && attempt.value != 0){
+        attempt.value--;
+        attempt.innerHTML = attempt.value;
+    }
+
+    else if(parseInt(attempt.value) == 0){
+        alert("You have no attepmt left!")
+    }
+
+    else if(numberArea.value == y){
+        attempt.innerHTML = attempt.value - 1
+    }
+})
